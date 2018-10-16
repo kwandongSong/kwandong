d13e6583d409800906298d6579a178f2d6b6ae54
Kwandong Song <gvc4456@hanmail.net>
2018-10-16 01:00:16 -0700

    added preprocessing

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #include"func1.h"
 #include"func2.h"
 #include<stdio.h>
+void preprocess()
+{
+  printf("do some preprocess! \n");
+}
 
 int main(void)
 {
+  preprocess();
   func1();
   func2();
   return 0;
