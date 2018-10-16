ac766fb4284eb9814bc83fe179eee7ea9e103f0c
Kwandong Song <gvc4456@hanmail.net>
2018-10-16 01:02:26 -0700

    added postprocessing

--- main.c
+++ main.c
@@ -6,9 +6,16 @@ void preprocess()
   printf("do some preprocess! \n");
 }
 
+void postprocess()
+{
+  printf("do some postprocess! \n");
+}
+
+
 int main(void)
 {
   preprocess();
+  postprocess();
   func1();
   func2();
   return 0;
