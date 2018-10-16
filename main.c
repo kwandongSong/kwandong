9831d13fce2e9940ef626ab064af098d139d454c
Kwandong Song <gvc4456@hanmail.net>
2018-10-16 00:56:46 -0700

    added turbo on/off

--- main.c
+++ main.c
@@ -2,6 +2,20 @@
 #include"func2.h"
 #include<stdio.h>
 
+void turbo_on()
+{
+  printf("turbo on! \n");
+
+}
+
+void turbo_off()
+{
+  printf("turbo off! \n");
+
+}
+
+
+
 int main(void)
 {
   func1();
